41f6d26bd0b6a12df55e86d030131d965f879272
And-Hobbes <cwkuner@gmail.com>
2026-05-02 18:46:44 -0500

    added content.js with writing, some animation added

---
+++ content.js
@@ -0,0 +1,31 @@
+const FILES = {
+	'AAIS.txt' : `BrundleCorp Internal Database Document:  e-7100
+	Version 1.4.2
+
+	Project - AuditaryAugmentationIntegrationSystem
+
+	... `,
+	'CVTS.txt' : `BrundleCorp Internal Database Document:	b12-ce19
+	Version 8.7.14
+
+	Project - ContextualVisualandTextualSimplifier
+
+	... `,
+
+	'AMI.txt' : `BrundleCorp Internal Database Document:	+3-18Ufor
+	Version 18.1.17
+
+	Project - ArtificialMnemonicIntegrator
+
+
+	...`,
+
+		'OpusPromethean.txt' : `BrundleCorp Internal Database Document: ██ ██ ██ ██ -4	
+	Version 1.1.██ █ 
+
+	Project - Opus Prometheam
+
+	...`
+};
+
+
