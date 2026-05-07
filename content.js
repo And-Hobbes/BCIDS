8aa94b08ef7d7d20694870ccc74f22c20bbb8216
Calvin <cwkuner@gmail.com>
2026-05-07 19:52:54 +0000

    testing new updates

--- content.js
+++ content.js
@@ -3,22 +3,22 @@ const FILES = {
 	Version 1.4.2
 
 	Project - AuditaryAugmentationIntegrationSystem®
-
+	---
 	The Auditary Augmentation Integrated System® is a auditory bypass implant placed between the tympanic membrane and the temporal lobe.\n\tVia this implant modulations to real inputs are possible, as well as the construction of fabricated noise within the recipients conciousness.\n\tFurthermore, free floating dopamine probes allow for the collection and analysis of prefered auditary stimulation, thus allowing maximum enjoyment of audio in all scenarios, by augment or obfuscation.
 
 	
-
+	---
 	Competition Report:
 	\n\tThe Penfield™ Mood-Organ® represents a large portion of market share in the limited abstraction emotion regulation market. Penfield™'s consumer base feels increasingly alienated from the present as a result artificial emotion regulation.
 	The AAIS allows constant increases in pleasure while retaining the "Be in the moment, be BrundleCorp"™ mantra we here at BC strive to embody.
 
-
+	---
 	Product RoadMap:
 	Human Trials at the Freeside research laboratory have demonstrated general success* with rare [but substantial] side effects.
 	Product is ready for general release, the 0.5% of patients that experience extreme side effects [Priapism, Loss of Smell, Onset of OCD] cannot be easily identified as resulting from the AAIS
 	Future development could include the sale of information of prefered noises or location dependent audio to enterprises to help entice users to purchase goods or services.
 
-
+	---
 	Marketing and Implementation Strategy:
 
 	DO NOT EXPOSE OR ADVERTISE COMPLETE AUDITORY SEPARATION FUNCTION.
@@ -26,12 +26,12 @@ const FILES = {
 	Advertising campaign demonstrating the connection to reality reacquired compared to the Mood-Organ®.
 
 	Celebrity Endorsement:
-	
+	---
 	We recommend the BrundleCorp™ acquire the digital likeness of "Mr.Damon" and "Ms.Williams"
 	These individuals propensity for perfidy of their supporters for ensure capital is well used.
 	Moreover, their recognition with film and health will support the idea of the augmentation of quality of life via the AAIS.
 
-
+	---
 	   !__!
 	  (@)(-)
 	 \\.'||'./
