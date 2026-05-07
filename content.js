0e62be4775e212a4798ba6bcdc79f775eef9e5cd
And-Hobbes <cwkuner@gmail.com>
2026-05-07 11:32:04 -0500

    more content added. still alot of work to do

--- content.js
+++ content.js
@@ -1,29 +1,38 @@
 const FILES = {
-	'AAIS.txt' : `BrundleCorp Internal Database Document:  e-7100
+	'AAIS.txt' : `BrundleCorp™ Internal Database Document:  e-7100
 	Version 1.4.2
 
-	Project - AuditaryAugmentationIntegrationSystem
+	Project - AuditaryAugmentationIntegrationSystem®
 
+	The Auditary Augmentation Integrated System® is a auditory bypass implant placed between the tympanic membrane and the temporal lobe.\n\tVia this implant modulations to real inputs are possible, as well as the construction of fabricated noise within the recipients conciousness.\n\tFurthermore, free dopamine probes allow for the collection and analysis of prefered auditary stimulation, thus allowing maximum enjoyment of audio in all scenarios, by augment or obfuscation.
+
+	\n\n 
+	Competition Report:
+	\n\tThe Penfield™ Mood-Organ® represents a large portion of market share in the limited abstraction emotion regulation market. Penfield™'s consumer base feels increasingly alienated from the present as a result artificial emotion regulation, The AAIS allows constant increases in pleasure while retaining the "Be in the moment, be BrundleCorp"™.
+
+	DO NOT EXPOSE OR ADVERTISE COMPLETE AUDITORY SEPARATION FUNCTION.
+
+	
 	... `,
-	'CVTS.txt' : `BrundleCorp Internal Database Document:	b12-ce19
+	'CVTS.txt' : `BrundleCorp™ Internal Database Document:	b12-ce19
 	Version 8.7.14
 
-	Project - ContextualVisualandTextualSimplifier
+	Project - ContextualVisualandTextualSimplifier® 
 
 	... `,
 
-	'AMI.txt' : `BrundleCorp Internal Database Document:	+3-18Ufor
+	'AMI.txt' : `BrundleCorp™ Internal Database Document:	+3-18Ufor
 	Version 18.1.17
 
-	Project - ArtificialMnemonicIntegrator
+	Project - ArtificialMnemonicIntegrator® 
 
 
 	...`,
 
-		'OpusPromethean.txt' : `BrundleCorp Internal Database Document: ██ ██ ██ ██ -4	
+		'OpusPromethean.txt' : `BrundleCorp™ Internal Database Document: ██ ██ ██ ██ -4	
 	Version 1.1.██ █ 
 
-	Project - Opus Prometheam
+	Project - Opus Prometheam® 
 
 	...`
 };
